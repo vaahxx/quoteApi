ba13095684b5007c42f7174024d1af7719ec614e
vaahxx <vaahxx@gmail.com>
2019-11-12 17:53:59 -0300

    get random quote done

--- server.js
+++ server.js
@@ -7,4 +7,16 @@ const { getRandomElement } = require('./utils');
 const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
+//app.use(router);
 
+app.get('/api/quotes/random', (req, res, next) => {
+    const randomQuote = {
+        quote: getRandomElement(quotes)
+    }
+    console.log(randomQuote);
+    res.status(200).send(randomQuote);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
