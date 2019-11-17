b5d72e57a9685c7660facad7e0cd55b80b67883f
vaahxx <vaahxx@gmail.com>
2019-11-16 23:58:10 -0300

    get quote by person and post quote

--- server.js
+++ server.js
@@ -7,14 +7,40 @@ const { getRandomElement } = require('./utils');
 const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
-//app.use(router);
 
+// sends a random quote 
 app.get('/api/quotes/random', (req, res, next) => {
     const randomQuote = {
         quote: getRandomElement(quotes)
     }
-    console.log(randomQuote);
     res.status(200).send(randomQuote);
+});
+
+app.get('/api/quotes', (req, res, next) =>{
+    // without query strings
+    if (!req.query.person) return res.status(200).send(quotes);
+    // with query strings
+    const filteredQuotes = quotes.filter(q => q.person === req.query.person);
+    res.status(200).send({
+        quotes: filteredQuotes
+    });
+});
+
+app.post('/api/quotes', (req, res, next) => {
+    // without query strings
+    if (!req.query.person || !req.query.quote) {
+        return res.status(400).send();    
+    }
+    // makes the new object, pushes it to the array and returns it 
+    const newQuote = { 
+        quote: req.query.quote,
+        person: req.query.person
+    }
+    quotes.push(newQuote);
+    return res.status(200).send({
+        quote: newQuote
+    })
+
 });
 
 app.listen(PORT, () => {
